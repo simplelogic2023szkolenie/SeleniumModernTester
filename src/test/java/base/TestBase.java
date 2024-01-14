58151c343cd5d838fc37bc77eca9aa37f94664cf
Mateusz Tadla <mat@scalepoint.com>
2024-01-14 12:30:56 +0100

    Initial commit

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,25 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("start-maximized");
+
+        // to zadziała bo od czerwca 2023, selenium ma wbudowanych
+        // webdrivermamanger -> automatycznie pobiera drivery do przegladarek
+        driver = new ChromeDriver(options);
+    }
+   @AfterMethod
+    public void cleanup(){
+        driver.quit();
+    }
+}
