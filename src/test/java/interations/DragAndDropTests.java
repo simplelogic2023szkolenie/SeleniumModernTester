02224f2416c352853036305f63963656a3c086cf
Mateusz Tadla <mat@scalepoint.com>
2024-01-14 16:56:35 +0100

    update

--- src/test/java/interations/DragAndDropTests.java
+++ src/test/java/interations/DragAndDropTests.java
@@ -1,7 +1,6 @@
 package interations;
 
 import base.TestBase;
-import com.beust.ah.A;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -10,25 +9,43 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class DragAndDropTests extends TestBase {
+    private WebElement draggable;
+    private WebElement areaToDrop;
+    private Actions actions;
+
     @BeforeMethod
-    public void openDragAndDropUrl(){
+    public void openDragAndDropUrl() {
         driver.get("http://seleniumui.moderntester.pl/droppable.php");
+        draggable = driver.findElement(By.cssSelector("#draggable"));
+        areaToDrop = driver.findElement(By.cssSelector("#droppable"));
+        actions = new Actions(driver);
     }
 
     @Test
-    public void shouldDragAndDropV1(){
-        WebElement draggable = driver.findElement(By.cssSelector("#draggable"));
-        WebElement areaToDrop = driver.findElement(By.cssSelector("#droppable"));
+    public void shouldDragAndDropV1() {
+        actions.dragAndDrop(draggable, areaToDrop)
+                .perform();
 
+        Assert.assertEquals(areaToDrop.getText(), "Dropped!");
+    }
 
-        Actions actions = new Actions(driver);
-
+    @Test
+    public void shouldDragAndDropV2() {
         actions.clickAndHold(draggable)
                 .moveToElement(areaToDrop)
                 .release()
                 .perform();
 
-        Assert.assertEquals(areaToDrop.getText(),"Dropped!");
+        Assert.assertEquals(areaToDrop.getText(), "Dropped!");
+    }
+
+    @Test
+    public void shouldDragAndDropV3() {
+        actions.clickAndHold(draggable)
+                .release(areaToDrop)
+                .perform();
+
+        Assert.assertEquals(areaToDrop.getText(), "Dropped!");
     }
 
 }
