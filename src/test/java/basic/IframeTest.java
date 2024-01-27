a91002dc70907e763fc5238d4d3fac0528ab28fb
Mateusz Tadla <mat@scalepoint.com>
2024-01-27 14:11:58 +0100

    update

---
+++ src/test/java/basic/IframeTest.java
@@ -0,0 +1,19 @@
+package basic;
+
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class IframeTest extends TestBase {
+
+    @Test
+    public void shouldFillFormInIframe(){
+        driver.get("http://automation-practice.emilos.pl/iframes.php");
+
+        driver.switchTo().frame("iframe1");
+        driver.findElement(By.cssSelector("#inputFirstName3")).clear();
+        driver.findElement(By.cssSelector("#inputFirstName3")).sendKeys("Jan");
+
+        driver.switchTo().defaultContent();
+    }
+}
