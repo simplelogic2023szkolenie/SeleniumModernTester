4441f13c5aa235f1013b2489e70cc4b490f57368
Mateusz Tadla <mat@scalepoint.com>
2024-01-27 16:44:34 +0100

    update

--- src/test/java/widgets/ProgressBarTests.java
+++ src/test/java/widgets/ProgressBarTests.java
@@ -5,23 +5,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class ProgressBarTests extends TestBase {
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void testSetup() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
+    }
 
     @Test
-    public void shouldWaitForProgressBar(){
-        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
-
+    public void shouldWaitForProgressBar() {
         String text = driver.findElement(By.xpath("//div[.='Complete!']")).getText();
 
         Assert.assertEquals(text, "Complete!");
     }
+
     @Test
-    public void shouldWaitForProgressBarV1(){
-        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
+    public void shouldWaitForProgressBarV1() {
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
@@ -33,9 +39,7 @@ public class ProgressBarTests extends TestBase {
     }
 
     @Test
-    public void shouldWaitForProgressBarV2(){
-        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
-
+    public void shouldWaitForProgressBarV2() {
         // implicitlyWait nigdy nie konfigurujemy bezpośrednio w teście tylko ustawiamy raz w TestBase
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -45,8 +49,7 @@ public class ProgressBarTests extends TestBase {
     }
 
     @Test
-    public void shouldWaitForProgressBarV3(){
-        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
+    public void shouldWaitForProgressBarV3() {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String text = driver.findElement(By.xpath("//div[.='iuhasdiuyhasdiuhasdiuhasd!']")).getText();
@@ -55,9 +58,19 @@ public class ProgressBarTests extends TestBase {
     }
 
     @Test
-    public void shouldWaitForProgressBarV4(){
-        driver.get("http://seleniumui.moderntester.pl/progressbar.php");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    public void shouldWaitForProgressBarV4() {
         wait.until(ExpectedConditions.textToBe(By.cssSelector(".progress-label"), "Complete!"));
+    }
+
+    @Test
+    public void shouldWaitForProgressBarV5() {
+        wait.until(ExpectedConditions.attributeContains(By.cssSelector(".ui-progressbar-value"), "class", "ui-progressbar-complete"));
+    }
+
+    @Test
+    public void shouldWaitForProgressBarV6() {
+        wait.until(d -> d.findElement(By.cssSelector(".ui-progressbar-value")).getAttribute("class").contains("complete"));
+
+        Assert.assertEquals(driver.findElement(By.cssSelector(".progress-label")).getText(), "Complete!");
     }
 }
