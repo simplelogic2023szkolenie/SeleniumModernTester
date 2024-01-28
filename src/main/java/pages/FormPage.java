4b15df7a24126481821e33806bcad345e2e207b3
Mateusz Tadla <mat@scalepoint.com>
2024-01-28 13:57:51 +0100

    update

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -1,5 +1,46 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
 public class FormPage {
+
+    public FormPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(css = "#inputFirstName3")
+    private WebElement firstNameInput;
+
+    @FindBy(css = "#selectContinents")
+    private WebElement continentsSelect;
+
+    @FindBy(css = "[name='gridCheckboxProfession']")
+    private List<WebElement> professionCheckboxes;
+
+    @FindBy(css = "#validator-message")
+    private WebElement validationLbl;
+
+    public void setFirstName(String firstName){
+        firstNameInput.sendKeys(firstName);
+    }
+
+    public void selectContinent(String continentValue){
+        new Select(continentsSelect).selectByValue(continentValue);
+    }
+
+    public void selectProfessions(int index){
+        professionCheckboxes.get(index).click();
+    }
+
+    public String getValidationMsg(){
+        return validationLbl.getText();
+    }
+
 
 }
--- src/test/java/basic/form/PopFormTest.java
+++ src/test/java/basic/form/PopFormTest.java
@@ -1,6 +1,23 @@
 package basic.form;
 
 import base.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.FormPage;
 
 public class PopFormTest extends TestBase {
+    @Test
+    public void shouldFillFormWithSuccess() {
+        driver.get("http://seleniumui.moderntester.pl/form.php");
+
+        FormPage formPage = new FormPage(driver);
+
+        formPage.setFirstName("Jan");
+
+        formPage.selectContinent("europe");
+        formPage.selectProfessions(1);
+
+
+        Assert.assertEquals(formPage.getValidationMsg(), "Form send with success");
+    }
 }
