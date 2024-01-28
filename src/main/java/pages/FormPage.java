adb57e27c7064c9b172495c96f3f479576326943
Mateusz Tadla <mat@scalepoint.com>
2024-01-28 15:06:35 +0100

    update

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 public class FormPage {
 
@@ -17,29 +19,124 @@ public class FormPage {
     @FindBy(css = "#inputFirstName3")
     private WebElement firstNameInput;
 
-    @FindBy(css = "#selectContinents")
-    private WebElement continentsSelect;
+    @FindBy(css = "#inputLastName3")
+    private WebElement lastNameInput;
 
-    @FindBy(css = "[name='gridCheckboxProfession']")
-    private List<WebElement> professionCheckboxes;
+    @FindBy(css = "#inputEmail3")
+    private WebElement emailInput;
+
+    @FindBy(css = "#inputAge3")
+    private WebElement ageInput;
 
     @FindBy(css = "#validator-message")
-    private WebElement validationLbl;
+    private WebElement validationMsg;
 
-    public void setFirstName(String firstName){
-        firstNameInput.sendKeys(firstName);
+    @FindBy(css = "#selectContinents")
+    private WebElement selectContinents;
+
+    @FindBy(css = "#selectSeleniumCommands")
+    private WebElement selectSeleniumCommands;
+    @FindBy(css = "#chooseFile")
+    private WebElement fileInput;
+
+    @FindBy(css = ".btn-primary")
+    private WebElement submitButton;
+
+    @FindBy(css = "[name='gridRadiosSex']")
+    private List<WebElement> gendersRadios;
+
+    @FindBy(css = "[name='gridRadiosExperience']")
+    private List<WebElement> experientceRadios;
+
+    @FindBy(css = "[name='gridCheckboxProfession']")
+    private List<WebElement> professionsCheckboxes;
+
+    public FormPage submitForm() {
+        submitButton.click();
+        return this;
     }
 
-    public void selectContinent(String continentValue){
-        new Select(continentsSelect).selectByValue(continentValue);
+    public FormPage selectProfessions(int index) {
+        professionsCheckboxes.get(index).click();
+        return this;
     }
 
-    public void selectProfessions(int index){
-        professionCheckboxes.get(index).click();
+    public FormPage selectRandomkExperiences() {
+        getRandomElement(experientceRadios).click();
+        return this;
     }
 
-    public String getValidationMsg(){
-        return validationLbl.getText();
+    public FormPage selectAllSeleniumCommands() {
+        List<WebElement> allSeleniumOptions = new Select(selectSeleniumCommands).getOptions();
+
+        for (WebElement option : allSeleniumOptions) {
+            option.click();
+        }
+        return this;
+    }
+
+    public FormPage selectAllSeleniumCommandsV2() {
+        int quantityOfOptions = new Select(selectSeleniumCommands).getOptions().size();
+
+        for (int i = 0; i < quantityOfOptions; i++) {
+            selectSeleniumCommand(i);
+        }
+        return this;
+    }
+
+    public FormPage selectSeleniumCommand(String seleniumCommand) {
+        new Select(selectSeleniumCommands).selectByValue(seleniumCommand);
+        return this;
+    }
+
+    public FormPage selectSeleniumCommand(int index) {
+        new Select(selectSeleniumCommands).selectByIndex(index);
+        return this;
+    }
+
+    public FormPage uploadFile(File file) {
+        fileInput.sendKeys(file.getAbsolutePath());
+        return this;
+    }
+
+    public FormPage selectContinent(String continent) {
+        new Select(selectContinents).selectByValue(continent);
+        return this;
+    }
+
+    public FormPage selectRandomGender() {
+        getRandomElement(gendersRadios).click();
+        return this;
+    }
+
+    public FormPage setFirstName(String firstName) {
+        this.firstNameInput.sendKeys(firstName);
+        return this;
+    }
+
+    public FormPage setLastName(String lastName) {
+        this.lastNameInput.sendKeys(lastName);
+        return this;
+    }
+
+    public FormPage setEmail(String email) {
+        this.emailInput.sendKeys(email);
+        return this;
+    }
+
+    public FormPage setAge(String age) {
+        this.ageInput.sendKeys(age);
+        return this;
+    }
+
+    public String getValidationMsg() {
+        return validationMsg.getText();
+    }
+
+    private WebElement getRandomElement(List<WebElement> elements) {
+        Random rnd = new Random();
+        int randomListIndex = rnd.nextInt(elements.size());
+        return elements.get(randomListIndex);
     }
 
 
--- src/test/java/basic/form/PopFormTest.java
+++ src/test/java/basic/form/PopFormTest.java
@@ -5,18 +5,28 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormPage;
 
+import java.io.File;
+
 public class PopFormTest extends TestBase {
     @Test
     public void shouldFillFormWithSuccess() {
         driver.get("http://seleniumui.moderntester.pl/form.php");
+        File file = new File("src/main/resources/someFile.txt");
 
         FormPage formPage = new FormPage(driver);
 
         formPage.setFirstName("Jan");
-
-        formPage.selectContinent("europe");
+        formPage.setLastName("Kowalski");
+        formPage.setEmail("j.k23@wp.pl");
+        formPage.setAge("23");
+        formPage.selectRandomGender();
         formPage.selectProfessions(1);
-
+        formPage.selectRandomkExperiences();
+        formPage.selectContinent("europe");
+        formPage.selectSeleniumCommand("browser-commands");
+        formPage.selectSeleniumCommand("switch-commands");
+        formPage.uploadFile(file);
+        formPage.submitForm();
 
         Assert.assertEquals(formPage.getValidationMsg(), "Form send with success");
     }
