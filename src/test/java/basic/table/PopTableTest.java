5fa4453a9e4c98f3cb09819a8fdf4423549a0a08
Mateusz Tadla <mat@scalepoint.com>
2024-01-28 16:56:55 +0100

    update

--- src/test/java/basic/table/PopTableTest.java
+++ src/test/java/basic/table/PopTableTest.java
@@ -2,11 +2,14 @@ package basic.table;
 
 import base.TestBase;
 import org.testng.annotations.Test;
+import pages.MountainsTablePage;
 
 public class PopTableTest extends TestBase {
 
     @Test
     public void printMountainsFromTableTest() {
+        driver.get("http://seleniumui.moderntester.pl/table.php");
 
+        new MountainsTablePage(driver).printMountainHigherThan(4000);
     }
 }
